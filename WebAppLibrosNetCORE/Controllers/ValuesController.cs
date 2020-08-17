47335623091f05f44581aee10d255292ae69f310
frank1916 <fparra@asesoftware.com>
2020-08-17 18:48:38 -0500

    cambio para probar desarrollo continuo

--- WebAppLibrosNetCORE/Controllers/ValuesController.cs
+++ WebAppLibrosNetCORE/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebAppLibrosNetCORE.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "VALUES 5" };
+            return new string[] { "value1", "value2", "VALUES 5" , "Values 6 para probar en azure production"};
         }
 
         // GET api/values/5
